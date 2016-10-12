4de58d613f94b8e4e50fd2c5b6569ee7ed1241cf
Neno Almeida <nenoxvx@icloud.com>
2016-10-11 21:58:15 -0300

    Using the fUsing the full_title helper in a test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,9 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  class ActiveSupport::TestCase
+    fixtures :all
+    include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
