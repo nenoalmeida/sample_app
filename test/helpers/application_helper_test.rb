fa76ddc008da3de50311a7ecc4bc49c04d877f97
Neno Almeida <nenoxvx@icloud.com>
2016-10-12 21:05:03 -0300

    Making tests pass

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
-
+# this file tests the full_title method itself
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title("Contact") contact_path
-    assert_equal full_title("Help"), help_path
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,18 +2,17 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
-    get root_path
-    assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-    get contact_path
-    assert_select "title", full_title("Contact")
-    get contact_path
-    assert_select "title", full_title("Help")
+    test "layout links" do
+        # verify signup page 5.4 #3
+        get signup_path
+        assert_response :success
+        assert_select "title", full_title("Sign up")
 
-
-  end
+        get root_path
+        assert_template 'static_pages/home'
+        assert_select "a[href=?]", root_path, count: 2
+        assert_select "a[href=?]", help_path
+        assert_select "a[href=?]", about_path
+        assert_select "a[href=?]", contact_path
+    end
 end
