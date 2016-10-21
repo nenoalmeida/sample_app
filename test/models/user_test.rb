68397459e264d91367c774e4cbf7fd84f44d845a
Neno Almeida <nenoxvx@icloud.com>
2016-10-20 22:22:32 -0200

    Commit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,13 +11,14 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-   @user.name = "     "
-   assert_not @user.valid?
- end
+    @user.name = ""
+    assert_not @user.valid?
+  end
 
- test "email should be present" do
-  @user.email = "     "
-  assert_not @user.valid?
-end
-
+  test "email addresses should be unique" do
+     duplicate_user = @user.dup
+     duplicate_user.email = @user.email.upcase
+     @user.save
+     assert_not duplicate_user.valid?
+   end
 end
